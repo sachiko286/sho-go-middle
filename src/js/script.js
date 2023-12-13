afe990ab960d95262f57e6e6af13a24a5e74cec5
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2023-12-13 23:22:56 +0900

    20231213

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,28 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-    // ハンバーガーメニュー
-    $(".js-hamburger").click(function () {
-        if($('.js-hamburger').hasClass('is-active')) {
-        $('.js-hamburger').removeClass("is-active");
-        //$("html").toggleClass("is-fixed");
-        $(".js-sp-nav").fadeOut(300);
-        } else {
-            $('.js-hamburger').addClass("is-active");
-            $(".js-sp-nav").fadeIn(300);
+    // ローディング画面
+    $(window).on("load", function () {
+        $("#loading").delay(800).fadeOut(600); // 画像をフェードアウト
+        $("#loadingWrap").delay(1000).fadeOut(600); // 背景色をフェードアウト
+        });
+        // ページの読み込みが完了しなくても2秒経ったら強制的にローディング画面をフェードアウト
+        setTimeout(stoploading, 2000);
+        function stoploading() {
+        $("#loadingWrap").fadeOut(600);
         }
-    });
+        
+        // ハンバーガーメニュー
+        $(".js-hamburger, .sp-nav__list-item a").click(function () {
+            if ($('.js-hamburger').hasClass('is-active')) {
+                $('.js-hamburger').removeClass("is-active");
+                //$("html").toggleClass("is-fixed");
+                $(".js-sp-nav").fadeOut(300);
+            } else {
+                $('.js-hamburger').addClass("is-active");
+                $(".js-sp-nav").fadeIn(300);
+            }
+        });
 
     // スワイパー
     const swiper = new Swiper(".swiper", {
