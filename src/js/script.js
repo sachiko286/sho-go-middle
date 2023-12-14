e240da3c6d2f21af274c2acee94d612cd4410be0
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2023-12-14 20:11:47 +0900

    20231214

--- src/js/script.js
+++ src/js/script.js
@@ -24,6 +24,27 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             }
         });
 
+        // スムーズスクロール
+        // href属性が"#"で始まるaタグがクリックされたときの処理
+        $('a[href^="#"]').click(function () {
+            // クリックされたaタグのhref属性の値を取得
+            var href = $(this).attr("href");
+            // 対象の要素を特定（hrefが"#"または空文字の場合はhtml要素をターゲットとする）
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            // ターゲットの位置（トップからの距離）を取得
+            var position = target.offset().top;
+            // スムーズスクロールの速度を定義
+            var speed = 500;
+        
+            // スムーズスクロールのアニメーション
+            $("html, body").animate({
+              scrollTop: position
+            }, speed, "swing");
+        
+            // aタグのデフォルトの動作（ページ遷移など）をキャンセル
+            return false;
+          });
+
     // スワイパー
     const swiper = new Swiper(".swiper", {
         loop: true,
